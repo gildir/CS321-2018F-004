fa6dbcafd310e63f3c4107bac6bfb470af281915
ishroom <kvemulap@gmu.edu>
2018-10-17 01:36:38 -0400

    ghost.java

---
+++ src/Ghost.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Ghost extends Thread{
+		String speech = "sample speech";
+		Player p;
+		
+		public Ghost(Player p){
+			this.p = p;
+		}
+
+		@Override
+		public void run() {
+			Random rn = new Random();
+			int i = rn.nextInt(100 - 1 + 1) + 1;
+			if (i >= 15) {
+			p.getReplyWriter().println("A ghost has appeared");
+			p.getReplyWriter().println(this.speech);
+			try {
+				Thread.currentThread().sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			p.getReplyWriter().println("the ghost disappears");
+			}
+		}
+	}
