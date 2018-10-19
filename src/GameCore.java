5447e7db7ccef4dddd43d4343d18acc0d0c1f930
TylerJurgens <tjurgen2@masonlive.gmu.edu>
2018-10-18 21:53:09 -0400

    forgot to uncomment something

--- src/GameCore.java
+++ src/GameCore.java
@@ -59,7 +59,7 @@ public class GameCore implements GameCoreInterface {
                 Random rand = new Random();
 				Room room = map.randomRoom();
 				ghoul = new Ghoul(room.getId());
-				//room.hasGhoul = true;
+				room.hasGhoul = true;
 				GameCore.this.broadcast(room, "You see a Ghoul appear in this room");
 
                 while(true) {
