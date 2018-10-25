876416c2aabe82b84df1ecff7c6d88af322eba06
Ahmed Ibrahim <aibrah23@gmu.edu>
2018-10-25 14:49:55 -0400

    805 fix again

--- src/GameCore.java
+++ src/GameCore.java
@@ -332,7 +332,7 @@ public class GameCore implements GameCoreInterface {
                 return "You reject " + playerChallenger.getName() + "\'s challenge to a R-P-S.";
             }
             else if(playerChallenger == playerChallengee)
-                return "You can't challenge yourself to R-P-S.";
+                return "You can't challenge yourself to R-P-S...";
             else {
                 return "This person is not in the same room as you or doesn't exist in the game.";
             }
