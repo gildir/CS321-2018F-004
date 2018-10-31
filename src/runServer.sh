bc32b062729f281c222d8403f7644ba9ef9b6e7a
Kapperchino <kamuran12580@gmail.com>
2018-10-31 00:32:27 -0400

    fixing more stuff

--- src/runServer.sh
+++ src/runServer.sh
@@ -1,6 +1,2 @@
 #! /bin/sh
-<<<<<<< HEAD
 rmiregistry & java -Djava.security.policy=game.policy GameServer localhost
-=======
-rmiregistry &; java -Djava.security.policy=game.policy GameServer localhost
->>>>>>> e70f57098d5e7734ee146d397b6066fd89ef4835
