5d0e6ced5cc1f7c023682153272e4daf2b83885c
Patrick Schemering <pschemer@gmu.edu>
2018-10-31 22:39:17 -0400

    removed duplicate methods from merge.  Note: a previous PR introduced a bug preventing the project from compiling. GameServer.java:47: error: unreported exception IOException; must be caught or declared to be thrown.

--- src/GameCore.java
+++ src/GameCore.java
@@ -816,36 +816,6 @@ public class GameCore implements GameCoreInterface {
 	}
 
 	/**
-     * Attempts to walk towards <direction> 1 time.  If unable to make it all the way,
-     *  a message will be returned.  Will display LOOK on any partial success.
-     * @param name Name of the player to move
-     * @param distance Number of rooms to move forward through.
-     * @return Message showing success.
-     */
-    public String move(String name, Direction direction) {
-        Player player = this.playerList.findPlayer(name);
-        if(player == null) {
-            return null;
-        }
-        Room room = map.findRoom(player.getCurrentRoom());
-        if(room.canExit(direction)) {
-            this.broadcast(player, player.getName() + " has walked off to the " + direction);
-            player.getReplyWriter().println(room.exitMessage(direction));
-            player.setCurrentRoom(room.getLink(direction));
-            String logMessage = String.format("%s used command MOVE %s [moved from %s to %s]", player.getName(), direction.toString(), room.getTitle(), map.findRoom(player.getCurrentRoom()).getTitle());
-			this.broadcast(player, player.getName() + " just walked into the area.");
-			Ghost g = new Ghost(player);
-				g.start();
-            player.getReplyWriter().println(this.map.findRoom(player.getCurrentRoom()).toString(playerList, player));
-        } else {
-            String logMessage  = String.format("%s used command MOVE %s [unable to move in direction]", player.getName(), direction.toString());
-            player.getReplyWriter().println(room.exitMessage(direction));
-            return "You grumble a little and stop moving.";
-        }
-        return "You stop moving and begin to stand around again.";
-	}
-	
-	/**
 	 * Attempts to pick up an object < target >. Will return a message on any
 	 * success or failure.
 	 * 
@@ -983,82 +953,6 @@ public class GameCore implements GameCoreInterface {
             return null;
         }
     }
-    
-
-    /**
-     * Attempts to erase the whiteboard in the room. Will return a message on any success or failure.
-     * @param name Name of the player to erase the whiteboard
-     * @return Message showing success.
-     */
-    public String whiteboardErase(String name) {
-        Player player = this.playerList.findPlayer(name);
-        if(player != null) {
-            Room room = map.findRoom(player.getCurrentRoom());
-            room.whiteboardErase();
-            this.broadcast(player, player.getName() + " erases the text on the whiteboard.");
-            dailyLogger.write(player.getName(), "WHITEBOARD ERASE", room.getTitle());
-            return "You erase the text on the whiteboard.";
-        }
-        else {
-            return null;
-        }
-    }
-
-    /**
-     * Attempts to read the whiteboard in the room. Will return a message on any success or failure.
-     * @param name Name of the player to erase the whiteboard
-     * @return Message showing success.
-     */
-    public String whiteboardRead(String name) {
-        Player player = this.playerList.findPlayer(name);
-        if(player != null) {
-            Room room = map.findRoom(player.getCurrentRoom());
-            String text = room.getWhiteboardText();
-            dailyLogger.write(player.getName(), "WHITEBOARD READ", room.getTitle());
-            this.broadcast(player, player.getName() + " reads the text on the whiteboard.");
-            if (text != null && !text.isEmpty()) {
-                return "The whiteboard says: \"" + text + "\".";
-            }
-            else {
-                return "The whiteboard is empty";
-            }
-
-        }
-        else {
-            return null;
-        }
-    }
-
-    /**
-     * Attempts to  the whiteboard in the room. Will return a message on any success or failure.
-     * @param name Name of the player to erase the whiteboard
-     * @param text Text to write on the whiteboard
-     * @return Message showing success.
-     */
-    public String whiteboardWrite(String name, String text) {
-        try {
-            Player player = this.playerList.findPlayer(name);
-            if(player != null) {
-                Room room = map.findRoom(player.getCurrentRoom());
-                boolean success = room.addWhiteboardText(text);
-                dailyLogger.write(player.getName(), "WHITEBOARD WRITE", text, room.getTitle());
-                if (success) {
-                    this.broadcast(player, player.getName() + " writes on the whiteboard.");
-                    return "You write text on the whiteboard.";
-                }
-                else {
-                    this.broadcast(player, player.getName() + " tries to write on the whiteboard, but it's full.");
-                    return "You try to write text on the whiteboard, but there's not enough space.";
-                }
-            }
-            else {
-                return null;
-            }
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-
 
     /**
 	 * Returns a string representation of all objects you are carrying.
