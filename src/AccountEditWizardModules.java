b1cba2a8d9a48d47d7a3cd176d1056bf610c05b4
whMason <42785643+whMason@users.noreply.github.com>
2018-11-12 08:54:37 -0500

    217 change password (#30)
    * Updates from 218
    
    * Added password changed confirmation
    
    * Fixed multiple options bug (#29)
    
    * 221 Feature

--- src/AccountEditWizardModules.java
+++ src/AccountEditWizardModules.java
@@ -46,8 +46,7 @@ public class AccountEditWizardModules {
 			stdout.print("Re-enter username: ");
 			String user = stdin.readLine().trim();
 			stdout.print("Re-enter password: ");
-			String pass = stdin.readLine().trim();
-			stdout.print("Enter new password: ");
+			String pass = new String(System.console().readPassword()).trim(); // task 221 hides password
 			Responses resp = this.obj.verifyPassword(user, pass);
 			if (resp != Responses.SUCCESS) {
 				switch (resp) {
@@ -62,7 +61,8 @@ public class AccountEditWizardModules {
 				}
 				return;
 			}
-			String newPass = stdin.readLine().trim();
+			stdout.print("Enter new password: ");
+			String newPass = new String(System.console().readPassword()).trim(); // task 221 hides password
 			resp = this.obj.resetPassword(user, newPass);
 			switch (resp) {
 			case NOT_FOUND:
