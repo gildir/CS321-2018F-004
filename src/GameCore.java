f0addeff8509e104d880601421df5c35f1d45792
spolisar <spolisar@gmu.edu>
2018-11-16 13:26:53 -0500

    modified message to display differently when a room is at capacity for items

--- src/GameCore.java
+++ src/GameCore.java
@@ -120,12 +120,13 @@ public class GameCore implements GameCoreInterface {
                         room = map.randomRoom();
                         try {
 							room.addObject(object);
+							GameCore.this.broadcast(room, "You see a student rush past and drop a " + object + " on the ground.");
 						}
 						catch (IndexOutOfBoundsException e){
+							GameCore.this.broadcast(room, "You see a student rush past.");
 						}
 
-						GameCore.this.broadcast(room, "You see a student rush past and drop a " + object + " on the ground.");
-						
+
                     } catch (InterruptedException ex) {
                         Logger.getLogger(GameObject.class.getName()).log(Level.SEVERE, null, ex);}
                 }}});
